b98c1531d075734abda342f59c3ec1cf8f9559ff
Vinay Bhat <vinaybhat001@gmail.com>
2015-07-22 18:34:33 +0530

    doubly linked list c

---
+++ FundamentalDataStructures/DoublyLinkedList/DoublyLinkedList.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+struct Node {
+  int element;
+  struct Node* next;
+  struct Node* prev;
+} *sentinel;
+
+sentinel->next = NULL;
+sentinel->prev = NULL;
+
+typedef struct Node* NODE;
+
+//NODE sentinel = (NODE) malloc (sizeof(struct Node));
+
+void addFront(int e) {
+  NODE n = (NODE) malloc (sizeof(struct Node));
+  n->element = e;
+  n->next = sentinel->next;
+  n->prev = sentinel;
+  sentinel->next->prev = n;
+  sentinel->next = n;
+}
+
+void display() {
+  NODE start = sentinel->next;
+  while (start->next != sentinel) {
+    printf("%d -> ", start->element);
+  }
+  printf("%d\n", start->element);
+}
+
+void main(int argc, char** argv) {
+  addFront(1);
+  addFront(2);
+  addFront(3);
+}
